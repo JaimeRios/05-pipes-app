30fe89b6c079a9a11b5b2338de4904aca3992dac
jaimeaRios <jaimearios1986@gmail.com>
2026-05-20 21:22:21 -0500

    UpperCase, LowerCase, TittleCase, Decimal, Percent, Currency, Dat, I18nSelect pipes, change idiom content projection from father to children

--- src/app/pages/basic-page/basic-page.ts
+++ src/app/pages/basic-page/basic-page.ts
@@ -1,9 +1,35 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Component, effect, inject, LOCALE_ID, signal } from '@angular/core';
+import { AvailableLocale, LocaleService } from '../../services/locale.service';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './basic-page.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class BasicPage {}
+export default class BasicPage {
+
+  localeService = inject(LocaleService);
+  currentLocale = signal(inject(LOCALE_ID))
+
+  nameLower = signal('Jaime');
+  nameUpper = signal('JAIME');
+  fullName = signal('JaIMe rIOs');
+
+  customDate = signal(new Date());
+
+  tickingDateEffect= effect((onCleanup)=>{
+    const interval = setInterval(()=>{
+      this.customDate.set(new Date())
+    },1000);
+
+    onCleanup(()=>{
+      clearInterval(interval);
+    })
+  });
+
+  changeLocale(locale: AvailableLocale){
+    this.localeService.changeLocale(locale);
+
+  }
+}
--- src/app/pages/uncommon-page/uncommon-page.ts
+++ src/app/pages/uncommon-page/uncommon-page.ts
@@ -1,8 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Card } from "../../components/card/card";
+import { I18nSelectPipe } from '@angular/common';
+
+const client1 = {
+  name: 'Jaime',
+  gender: 'male',
+  age: 39,
+  address: 'Medellin, Colombia'
+}
+
+const client2 = {
+  name: 'Mayumi',
+  gender: 'female',
+  age: 33,
+  address: 'Tokyo, Japan'
+}
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [],
+  imports: [Card, I18nSelectPipe],
   templateUrl: './uncommon-page.html',
 })
-export default class UncommonPage {}
+export default class UncommonPage {
+
+  //i18n Select
+  client = signal(client1);
+
+  invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  }
+
+  changeClient(){
+    if(this.client() == client1){
+      this.client.set(client2)
+      return;
+    }
+
+    this.client.set(client1);
+  }
+
+}
